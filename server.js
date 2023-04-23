8d826447dfc26cdf88c902ad5888b48bc97f5f02
liberty <leeamson@gmail.com>
2023-04-23 19:43:51 +0200

    message edit

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const ENV = 'DEV';
 const app = express();
 app.get('/', (req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello from from Primus Learning, this was awesome';
+  const msg = 'This is an Awesome project';
   res.send(getPage(msg));
 });
 
